9b6948ae4c48453a22fbc4fdff3232686b8f9164
Thematias270 <148594739+Thematias270@users.noreply.github.com>
2025-05-11 20:40:53 -0300

    tp01 - Entidad desarrollada

---
+++ src/main/java/com/imb2025/smedico/service/IHistorialPacienteService.java
@@ -0,0 +1,17 @@
+package com.imb2025.smedico.service;
+
+import java.util.List;
+
+import com.imb2025.smedico.entity.HistorialPaciente;
+
+
+
+public interface IHistorialPacienteService {
+	
+	public List<HistorialPaciente> findAll();
+	public HistorialPaciente findById(Long id);
+	public HistorialPaciente save(HistorialPaciente historial);
+	public void deleteById(Long id);
+	HistorialPaciente update(Long id, HistorialPaciente historial);
+
+}
