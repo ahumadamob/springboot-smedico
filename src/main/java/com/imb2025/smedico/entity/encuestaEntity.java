bd36d43879351baf40def5ed2a746ec80484e73e
Humberto Jordan <jordanhmza@outlook.com>
2025-05-11 21:52:08 -0300

    TP01 - Entidad encuesta desarrollada

---
+++ src/main/java/com/imb2025/smedico/entity/encuestaEntity.java
@@ -0,0 +1,57 @@
+package com.imb2025.smedico.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class encuestaEntity {
+       
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	//Creamos el Objeto
+	
+	private Long id;
+	private Long pacienteId;
+	private Long consultaId;
+	private int puntaje;
+	private String comentario;
+	
+	
+	//GET - SET
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getPacienteId() {
+		return pacienteId;
+	}
+	public void setPacienteId(Long pacienteId) {
+		this.pacienteId = pacienteId;
+	}
+	public Long getConsultaId() {
+		return consultaId;
+	}
+	public void setConsultaId(Long consultaId) {
+		this.consultaId = consultaId;
+	}
+	public int getPuntaje() {
+		return puntaje;
+	}
+	public void setPuntaje(int puntaje) {
+		this.puntaje = puntaje;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	
+	
+	
+}
