e40134baf0820bf7107b7841ff82bab19b3c1abf
lautaro321 <skoollarris9@gmail.com>
2025-05-13 14:12:44 -0300

    TP01 - Entidad desarrollada

---
+++ src/main/java/com/imb2025/smedico/repositories/DetalleFacturaRepositories.java
@@ -0,0 +1,16 @@
+
+
+
+package com.imb2025.smedico.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.imb2025.smedico.entity.DetalleFactura;
+
+
+
+public interface DetalleFacturaRepositories extends JpaRepository<DetalleFactura, Long> {
+    
+
+
+};
