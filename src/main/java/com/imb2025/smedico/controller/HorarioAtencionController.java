a2d274e7a092b1f4684967e25964b6bb7104deec
Mario Ahumada <ahumadamob@gmail.com>
2025-05-14 19:13:53 -0300

    Tp01 gonzalez jennifer fix (#35)
    * Correcciones del profesor
    
    * Correcciones del Profesor

--- src/main/java/com/imb2025/smedico/controller/HorarioAtencionController.java
+++ src/main/java/com/imb2025/smedico/controller/HorarioAtencionController.java
@@ -5,34 +5,34 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import com.imb2025.smedico.entity.HorarioAtencionEntity;
-import com.imb2025.smedico.service.HorarioAtencionService;
+import com.imb2025.smedico.entity.HorarioAtencion;
+import com.imb2025.smedico.service.IHorarioAtencionService;
 
 @RestController
 @RequestMapping("/horarioAtencion")
 public class HorarioAtencionController {
 
     @Autowired
-    private HorarioAtencionService horarioAtencionService;
+    private IHorarioAtencionService horarioAtencionService;
 
     @GetMapping
-    public List<HorarioAtencionEntity> getAllHorarioAtencion() {
+    public List<HorarioAtencion> getAllHorarioAtencion() {
         return horarioAtencionService.getAllHorarioAtencion();
     }
 
     @GetMapping("/{id}")
-    public HorarioAtencionEntity getHorarioAtencionById(@PathVariable Long id) {
+    public HorarioAtencion getHorarioAtencionById(@PathVariable Long id) {
         return horarioAtencionService.getHorarioAtencionById(id);
     }
 
     @PostMapping
-    public HorarioAtencionEntity createHorarioAtencion(@RequestBody HorarioAtencionEntity horarioAtencionEntity) {
-        return horarioAtencionService.createHorarioAtencion(horarioAtencionEntity);
+    public HorarioAtencion createHorarioAtencion(@RequestBody HorarioAtencion horarioAtencionEntity) {
+        return horarioAtencionService.save(horarioAtencionEntity);
     }
 
     @PutMapping("/{id}")
-    public HorarioAtencionEntity updateHorarioAtencion(@PathVariable Long id, @RequestBody HorarioAtencionEntity horarioAtencionEntity) {
-        return horarioAtencionService.updateHorarioAtencion(id, horarioAtencionEntity);
+    public HorarioAtencion updateHorarioAtencion(@PathVariable Long id, @RequestBody HorarioAtencion horarioAtencionEntity) {
+        return horarioAtencionService.save(horarioAtencionEntity);
     }
     
     @DeleteMapping("/{id}")
--- src/main/java/com/imb2025/smedico/entity/HorarioAtencion.java
+++ src/main/java/com/imb2025/smedico/entity/HorarioAtencion.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class HorarioAtencionEntity{
+public class HorarioAtencion{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
