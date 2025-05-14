f8a680b441ea66908cb64b540d0c299296625709
Martina <fmartina511@gmail.com>
2025-05-13 22:45:13 -0300

    Actualizacion del codigo

--- src/main/java/com/imb2025/smedico/service/IPacienteService.java
+++ src/main/java/com/imb2025/smedico/service/IPacienteService.java
@@ -5,11 +5,11 @@ import com.imb2025.smedico.entity.Paciente;
 import java.util.List;
 import java.util.Optional;
 
-public interface PacienteService {
+public interface IPacienteService {
 
     List<Paciente> findAll();
 
-    Optional<Paciente> findById(Long id);
+    Paciente findById(Long id);
 
     Paciente save(Paciente paciente);
 
