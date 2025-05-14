0d35a01cadb177f6426e1aff7ce86d21202c98ff
Germann <137139675+GermanGiorgis@users.noreply.github.com>
2025-05-14 16:47:21 -0300

    TP01 - Entidad desarrollada (#30)
    * TP01 - Entidad desarrollada
    
    * Correcciones del profesor
    
    ---------
    
    Co-authored-by: Mario Ahumada <ahumadamob@gmail.com>

---
+++ src/main/java/com/imb2025/smedico/entity/EstadoTurno.java
@@ -0,0 +1,36 @@
+package com.imb2025.smedico.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "estado_turno") // Especifica el nombre de la tabla en la BD
+public class EstadoTurno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Generación automática del ID
+    private Long id;
+    private String nombre;
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
