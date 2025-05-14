c94a9d3693f2e3aaaed0b22d2eb17adafb722b53
Mario Ahumada <ahumadamob@gmail.com>
2025-05-14 15:40:07 -0300

    Correcciones del profesor

--- src/main/java/com/imb2025/smedico/controller/HistorialPacienteController.java
+++ src/main/java/com/imb2025/smedico/controller/HistorialPacienteController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +39,11 @@ public class HistorialPacienteController {
     public HistorialPaciente create(@RequestBody HistorialPaciente historial) {
         return service.save(historial);
     }
+    
+    @PutMapping("/")
+    public HistorialPaciente update(@RequestBody HistorialPaciente historial) {
+        return service.save(historial);
+    }    
 
     //DELETE: Eliminar historial por ID
     @DeleteMapping("/{idhistorialpaciente}")
