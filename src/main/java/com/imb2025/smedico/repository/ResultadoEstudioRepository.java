3ac3f1a8a54ef43bb8fdd2167dca4e16d9ceb385
Mario Ahumada <ahumadamob@gmail.com>
2025-05-14 20:49:42 -0300

    TP01 - tp1 desarrollado, ResultadoEstudio (#37)
    Co-authored-by: francisco ruiz <franciscojoseruiz19@gmail.com>

---
+++ src/main/java/com/imb2025/smedico/repository/ResultadoEstudioRepository.java
@@ -0,0 +1,12 @@
+package com.imb2025.smedico.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.imb2025.smedico.entity.ResultadoEstudio;
+
+public interface ResultadoEstudioRepository extends JpaRepository<ResultadoEstudio, Long>{
+
+
+}
