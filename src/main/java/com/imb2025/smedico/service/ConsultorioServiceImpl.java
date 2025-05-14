12f44df1bfbb1adfe8863fc4dd5d3262dcfb8b2c
Marianela Zapata <marianelanoelizapatacardinali@gmail.com>
2025-05-13 21:15:20 -0300

    Consultorio TP 01 terminado

---
+++ src/main/java/com/imb2025/smedico/service/ConsultorioServiceImpl.java
@@ -0,0 +1,59 @@
+package com.imb2025.smedico.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.imb2025.smedico.entity.Consultorio;
+import com.imb2025.smedico.repository.ConsultorioRepository;
+
+@Service
+public class ConsultorioServiceImpl implements IConsultorioService {
+
+	@Autowired
+	private ConsultorioRepository repository;
+	
+	//Crear y guardar nuevo repositorio
+	@Override
+	public Consultorio guardarConsultorio(Consultorio consultorio) {
+		return repository.save(consultorio);
+	}
+	
+	//Buscar por id
+	 @Override
+	 public Consultorio buscarPorId(Long id) {
+		 Optional<Consultorio> opt;
+			opt = repository.findById(id);
+			if(opt.isPresent()) {
+				return opt.get();
+			}else {
+				return null;
+		 }
+	 }
+	
+	//Buscar por nombre
+	@Override
+    public List<Consultorio> buscarPorNombre(String nombre) {
+        return repository.findByNombre(nombre);
+    }
+	
+	//Listar todos
+	@Override
+    public List<Consultorio> listarTodos() {
+	    return repository.findAll();
+	}
+	
+	// Eliminar por ID
+    @Override
+    public void eliminarPorId(Long id) {
+        repository.deleteById(id);
+    }
+    
+    //Eliminar por nombre
+    @Override
+    public void eliminarPorNombre(String nombre) {
+        repository.deleteByNombre(nombre);
+    }
+	
+}
