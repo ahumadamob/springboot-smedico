5ef4893add65e315124a3d5633171dbb76404364
Mario Ahumada <ahumadamob@gmail.com>
2025-05-14 22:11:59 -0300

    Correcciones del Profesor

--- src/main/java/com/imb2025/smedico/service/IDetalleRecetaService.java
+++ src/main/java/com/imb2025/smedico/service/IDetalleRecetaService.java
@@ -3,12 +3,10 @@ package com.imb2025.smedico.service;
 import com.imb2025.smedico.entity.DetalleReceta;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface DetalleRecetaService {
+public interface IDetalleRecetaService {
     List<DetalleReceta> findAll();
-    Optional<DetalleReceta> findById(Long id);
+    DetalleReceta findById(Long id);
     DetalleReceta save(DetalleReceta recetaMedicamento);
-    DetalleReceta update(Long id, DetalleReceta recetaMedicamento);
     void deleteById(Long id);
 }
