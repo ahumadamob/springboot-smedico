b7447021ff5cf70cc7313c7c565244e13a3f380c
Mario Ahumada <ahumadamob@gmail.com>
2025-05-14 21:08:27 -0300

    Correcciones del profesor

--- src/main/java/com/imb2025/smedico/service/IDiagnosticoService.java
+++ src/main/java/com/imb2025/smedico/service/IDiagnosticoService.java
@@ -3,12 +3,10 @@ package com.imb2025.smedico.service;
 import com.imb2025.smedico.entity.Diagnostico;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IDiagnosticoService {
     List<Diagnostico> findAll();
-    Optional<Diagnostico> findById(Long id);
+    Diagnostico findById(Long id);
     Diagnostico save(Diagnostico diagnostico);
-    Diagnostico update(Long id, Diagnostico diagnostico);
     void deleteById(Long id);
 }
