91d275c7549b05b82f009bfc97a91f63d047d85b
Angel418 <angelpuche01@gmail.com>
2025-05-26 01:08:47 -0300

    TP02 - DTO implementado

--- src/main/java/com/imb2025/smedico/service/IMotivoCancelacionService.java
+++ src/main/java/com/imb2025/smedico/service/IMotivoCancelacionService.java
@@ -2,12 +2,14 @@ package com.imb2025.smedico.service;
 
 import java.util.List;
 
+import com.imb2025.smedico.dto.MotivoCancelacionRequestDTO;
 import com.imb2025.smedico.entity.MotivoCancelacion;
 
 public interface IMotivoCancelacionService {
 	
 	public List<MotivoCancelacion> findAll();
 	public MotivoCancelacion findById(Long id);
-	public MotivoCancelacion save(MotivoCancelacion motivocancelacion);
+	public MotivoCancelacion create(MotivoCancelacionRequestDTO dto);
+	public MotivoCancelacion update(Long id, MotivoCancelacionRequestDTO dto) throws Exception;;
 	public void deleteById(Long id);
 }
