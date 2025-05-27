88e8f4d54b74132fa02f2f64e7dceaece81f4efb
Scomes02 <comessantiago@gmail.com>
2025-05-26 21:22:30 -0300

    TP02 - Relaciones y DTO implementados

--- src/main/java/com/imb2025/smedico/controller/AsistenteController.java
+++ src/main/java/com/imb2025/smedico/controller/AsistenteController.java
@@ -1,10 +1,14 @@
 package com.imb2025.smedico.controller;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.imb2025.smedico.dto.AsistenteRequestDTO;
 import com.imb2025.smedico.entity.Asistente;
 import com.imb2025.smedico.service.IAsistenteService;
 
@@ -26,13 +30,13 @@ public class AsistenteController {
     }
 
     @PostMapping
-    public Asistente save(@RequestBody Asistente asistenteEntity) {
-        return service.save(asistenteEntity);
+    public Asistente save(@RequestBody AsistenteRequestDTO dto) {
+        return service.create(dto);
     }
 
     @PutMapping("/{id}")
-    public Asistente update(@PathVariable("id") Long id, @RequestBody Asistente asistenteActualizado) {
-        return service.update(id, asistenteActualizado);
+    public Asistente update(@PathVariable("id") Long id, @RequestBody AsistenteRequestDTO dto) {
+        return service.update(id, dto);
     }
 
     @DeleteMapping("/{id}")
@@ -40,6 +44,11 @@ public class AsistenteController {
         service.deleteById(id);
         return "Asistente con ID " + id + " eliminado correctamente";
     }
-
+    
+    @ExceptionHandler(RuntimeException.class)
+    public Map<String, Object> handleRuntimeException(RuntimeException ex) {
+        Map<String, Object> errorMap = new HashMap<>();
+        errorMap.put("message", ex.getMessage());
+        return errorMap;
+    }
 }
-
--- src/main/java/com/imb2025/smedico/service/IAsistenteService.java
+++ src/main/java/com/imb2025/smedico/service/IAsistenteService.java
@@ -1,14 +1,14 @@
 package com.imb2025.smedico.service;
 
 import java.util.List;
-
+import com.imb2025.smedico.dto.AsistenteRequestDTO;
 import com.imb2025.smedico.entity.Asistente;
 
 public interface IAsistenteService {
 
-	public List<Asistente> findAll();
-	public Asistente findById(Long id);
-	public Asistente save(Asistente asistenteentity);
-	public void deleteById(Long id);
-	public Asistente update(Long id, Asistente asistenteActualizado);
+    public List<Asistente> findAll();
+    public Asistente findById(Long id);
+    public Asistente create(AsistenteRequestDTO asistenteDTO);
+    public void deleteById(Long id);
+    public Asistente update(Long id, AsistenteRequestDTO asistenteDTO);
 }
