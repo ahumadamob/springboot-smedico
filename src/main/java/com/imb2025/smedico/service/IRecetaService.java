f653a2b2a0799ea73b88da3821224d36cc166d72
Mauricio Aiello <137466562+MauricioAiello99@users.noreply.github.com>
2025-06-02 23:31:47 -0300

    TP02 -Relaciones y DTO implementados (#67)
    * TP02 -Relaciones y DTO implementados
    
    * TP02 fix

--- src/main/java/com/imb2025/smedico/service/IRecetaService.java
+++ src/main/java/com/imb2025/smedico/service/IRecetaService.java
@@ -2,13 +2,17 @@ package com.imb2025.smedico.service;
 
 import java.util.List;
 
+import com.imb2025.smedico.dto.RecetaRequestDTO;
 import com.imb2025.smedico.entity.Receta;
+
+import imb.progra2025.p3ro2da.dto.AlumnoRequestDTO;
+import imb.progra2025.p3ro2da.entity.Alumno;
 
 public interface IRecetaService {
 	public List <Receta> findAll();
 	public Receta findById(Long id);
-	public Receta save (Receta receta);
+	public Receta create(Receta receta);
+    public Receta update(Long id,Receta receta) throws Exception;
 	public void deleteById (Long id);
-	
-
+	public Receta fromDto(RecetaRequestDTO requestDto) throws Exception;
 }
