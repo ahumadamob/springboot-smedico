2d6c9463cb38b4e2caa84f27367085c29f16ed95
Nicolás Díaz <91564857+N1CKWEB@users.noreply.github.com>
2025-06-06 12:25:53 -0300

    TP02 - Relaciones y DTO implementados (#55)
    * TP02 - Relaciones y DTO implementados
    
    * Arreglando caracteres Unicode

--- src/main/java/com/imb2025/smedico/entity/OrdenEstudio.java
+++ src/main/java/com/imb2025/smedico/entity/OrdenEstudio.java
@@ -1,6 +1,5 @@
 package com.imb2025.smedico.entity;
 
-
 import java.time.LocalDate;
 
 import jakarta.persistence.*;
@@ -8,42 +7,74 @@ import jakarta.persistence.*;
 @Entity
 public class OrdenEstudio {
 
-	// OrdenEstudio: id,fecha,medicoId,pacienteId, estudioId
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private LocalDate fecha;
-	private Long medicoId;
-	private Long pacienteId;	
-	private Long estudioId;
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	// Getters y Setters
-	public LocalDate getFecha() {
-		return fecha;
+    private LocalDate fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "medicoId") 
+    private Medico medico;
+
+    @ManyToOne
+    @JoinColumn(name = "pacienteId")
+    private Paciente paciente;
+
+    @ManyToOne
+    @JoinColumn(name = "estudioId")
+    private EstudioEntity estudio;
+    
+    // Constructor 
+    public OrdenEstudio() {}
+    
+    
+    public OrdenEstudio(LocalDate fecha, Medico medico,Paciente paciente, EstudioEntity estudio) {
+    	this.fecha=fecha;
+    	this.paciente=paciente;
+    	this.medico=medico;
+    	this.estudio=estudio;
+    }
+    
+        
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public Medico getMedico() {
+		return medico;
 	}
+
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public EstudioEntity getEstudio() {
+		return estudio;
+	}
+
+	public void setEstudio(EstudioEntity estudio) {
+		this.estudio = estudio;
+	}
+
 	public void setFecha(LocalDate fecha) {
-		this.fecha = fecha;
-	}
-	public Long getMedicoId() {
-		return medicoId;
-	}
-	public void setMedicoId(Long medicoId) {
-		this.medicoId = medicoId;
-	}
-	public Long getPacienteId() {
-		return pacienteId;
-	}
-	public void setPacienteId(Long pacienteId) {
-		this.pacienteId = pacienteId;
-	}
-	public Long getEstudioId() {
-		return estudioId;
-	}
-	public void setEstudioId(Long estudioId) {
-		this.estudioId = estudioId;
-	}
-
-	
+        this.fecha = fecha;
+    }
 }
