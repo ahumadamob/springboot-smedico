92ff6eec5bb2dd0a7b74f85bd16a07dc51f1dc6b
Enzo Juri <149835978+Rafit98@users.noreply.github.com>
2025-06-10 20:59:29 -0300

    Delete smedico1/src/main/java/com/imb2025/smedico/repository/EspecialidadRepository.java

--- smedico1/src/main/java/com/imb2025/smedico/repository/EspecialidadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imb2025.smedico.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.imb2025.smedico.entity.Especialidad;
-
-public interface EspecialidadRepository extends JpaRepository<Especialidad, Long> {
-
-}
