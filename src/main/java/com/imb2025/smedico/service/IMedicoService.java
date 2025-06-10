6192b63549b55f6860ee1862bad76d613c24de6a
Mauro Barrera <164958377+MaurooB23@users.noreply.github.com>
2025-06-10 18:53:21 -0300

    TPO2 - Relaciones y DTO implmentados (#70)
    * TPO2 - Relaciones y DTO implmentados
    
    * TP02 - Relaciones y DTO implementados
    
    * correcciones en serviceImpl y entidad Medico

--- src/main/java/com/imb2025/smedico/service/IMedicoService.java
+++ src/main/java/com/imb2025/smedico/service/IMedicoService.java
@@ -2,13 +2,15 @@ package com.imb2025.smedico.service;
 
 import java.util.List;
 
+import com.imb2025.smedico.dto.MedicoRequestDTO;
 import com.imb2025.smedico.entity.Medico;
 
 public interface IMedicoService {
-	
-	public List <Medico> findAll();
-	public Medico findById(Long id);
-	public Medico save(Medico medico);
-	public void deleteById(Long id); 
+    List<Medico> findAll();
+    Medico findById(Long id);
+    void deleteById(Long id);
 
+    Medico fromDto(MedicoRequestDTO dto) throws Exception;
+    Medico create(Medico medico);
+    Medico update(Long id, Medico medico) throws Exception;
 }
