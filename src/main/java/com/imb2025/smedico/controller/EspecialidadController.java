332d3aff6f1a5b821b02f22a13a4e657e90bb783
Gastón Ruiz <135028423+ggastonruiz@users.noreply.github.com>
2025-06-18 21:41:46 -0300

    TP02 - Relaciones y DTO implementados (#83)
    * TP02 - Relaciones y DTO implementados
    
    * DTO subido
    
    * TP02 - Cambios terminados
    
    ---------
    
    Co-authored-by: Gaston Fran <gastonjose16@gmail.com>

--- src/main/java/com/imb2025/smedico/controller/EspecialidadController.java
+++ src/main/java/com/imb2025/smedico/controller/EspecialidadController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.imb2025.smedico.entity.Especialidad;
 import com.imb2025.smedico.service.IEspecialidadService;
 
+import dto.EspecialidadRequestDTO;
+
 @RestController
 public class EspecialidadController {
 	
@@ -32,18 +34,31 @@ public class EspecialidadController {
 	}
 	
 	@PostMapping("/especialidad")
-	public Especialidad createEspecialidad(@RequestBody Especialidad especialidad) {
-		return service.save(especialidad);
+	public Especialidad create(@RequestBody EspecialidadRequestDTO dto) {
+		try {
+			Especialidad entity = service.fromDto(dto);
+	            return service.create(entity);
+		}catch(Exception e){
+			e.printStackTrace();
+            return null;
+		}
 	}
 	 
-	@PutMapping("/especialidad")
-	public Especialidad updateEspecialidad(@RequestBody Especialidad especialidad) {
-		return service.save(especialidad);
+	@PutMapping("/especialidad/{idespecialidad}")
+	public Especialidad update(@RequestBody EspecialidadRequestDTO dto, @PathVariable("idespecialidad") Long id) {
+		try {
+			 Especialidad entity = service.fromDto(dto);
+			 return service.update(id, entity);
+		}catch (Exception e){
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 	
 	@DeleteMapping("/especialidad/{idespecialidad}")
 	public String deleteEspecialidad(@PathVariable("idespecialidad") Long id) {
 		service.deleteById(id);
-		return "Especialidad " +id.toString() + "eliminada correctamente. ";
+		return "Especialidad " +id.toString() + " Eliminada correctamente. ";
 	}
 }
