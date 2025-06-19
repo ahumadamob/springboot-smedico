0c5e08c6b45053688ef1435ce03f4b22a898a4d6
Oscar Naselli <165207564+OskNas7@users.noreply.github.com>
2025-06-18 22:16:33 -0300

    TP02 - Relacion y DTO (Mediciamento) Implementado (#76)
    * TP02 - Relacion y DTO (Mediciamento) Implementado
    
    * tp02-medicamentoDTO agregado
    
    * TP02- correccion de metodos

--- src/main/java/com/imb2025/smedico/dto/MedicamentoRequestDTO.java
+++ src/main/java/com/imb2025/smedico/dto/MedicamentoRequestDTO.java
@@ -1,56 +1,42 @@
-package com.imb2025.smedico.entity;
+package com.imb2025.smedico.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
-public class Medicamento {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+public class MedicamentoRequestDTO {
 	private String nombre;
 	private String presentacion;
 	private String dosisSugerida;
 	
-
-	//Constructor vacio
-	public Medicamento() {}
-	
-	//Contructor con los campos
-	public Medicamento(String nombre, String presentacion, String dosisSugerida) {
+	public MedicamentoRequestDTO(String nombre, String presentacion, String dosisSugerida) {
 		super();
 		this.nombre = nombre;
 		this.presentacion = presentacion;
 		this.dosisSugerida = dosisSugerida;
+
 	}
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getPresentacion() {
 		return presentacion;
 	}
+
 	public void setPresentacion(String presentacion) {
 		this.presentacion = presentacion;
 	}
+
 	public String getDosisSugerida() {
 		return dosisSugerida;
 	}
+
 	public void setDosisSugerida(String dosisSugerida) {
 		this.dosisSugerida = dosisSugerida;
 	}
+
 	
-	
+
 }
--- src/main/java/com/imb2025/smedico/service/jpa/MedicamentoServiceImpl.java
+++ src/main/java/com/imb2025/smedico/service/jpa/MedicamentoServiceImpl.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.imb2025.smedico.dto.MedicamentoRequestDTO;
 import com.imb2025.smedico.entity.Medicamento;
 import com.imb2025.smedico.repository.MedicamentoRepository;
 import com.imb2025.smedico.service.IMedicamentoService;
+
 
 @Service
 public class MedicamentoServiceImpl implements IMedicamentoService{
@@ -32,13 +34,34 @@ public class MedicamentoServiceImpl implements IMedicamentoService{
 	}
 	
 	@Override
-	public Medicamento save(Medicamento medicamento) {
-		return repoMedic.save(medicamento);
+	public Medicamento create(Medicamento medicamento) throws Exception{
+		return repoMedic.save(medicamento);	
+	}
+	
+	
+	@Override
+	public Medicamento update(Long id, Medicamento medicamento) throws Exception{
+		 Optional<Medicamento> opt = repoMedic.findById(id);
+		    if (opt.isPresent()) {
+		    	
+		        medicamento.setNombre(medicamento.getNombre());
+		        medicamento.setDosisSugerida(medicamento.getDosisSugerida());
+		        medicamento.setPresentacion(medicamento.getPresentacion());
+		        return repoMedic.save(medicamento);
+		    } else {
+		    	throw new Exception("El medicamento con ID " + id + " no existe.");
+		    }	
 	}
 
 	@Override
 	public void deleteById(Long id) {
 		repoMedic.deleteById(id);		
+	}
+	
+	@Override
+	public Medicamento fromDto(MedicamentoRequestDTO dto) throws Exception {
+		
+	    return new Medicamento(dto.getNombre(), dto.getDosisSugerida(), dto.getPresentacion());
 	}
 	
 
