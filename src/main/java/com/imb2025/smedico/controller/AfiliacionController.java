f41e4a1c784ed942193571c99c044d8ac7199980
Enzo Juri <149835978+Rafit98@users.noreply.github.com>
2025-06-24 20:40:43 -0300

    Update AfiliacionController.java

--- src/main/java/com/imb2025/smedico/controller/AfiliacionController.java
+++ src/main/java/com/imb2025/smedico/controller/AfiliacionController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.imb2025.smedico.DTO.AfiliacionRequestDTO;
+import com.imb2025.smedico.dto.AfiliacionRequestDTO;
 
 import com.imb2025.smedico.entity.Afiliacion;
 import com.imb2025.smedico.service.IAfiliacionService;
