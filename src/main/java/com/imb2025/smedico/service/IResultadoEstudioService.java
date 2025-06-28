33bf8191fb256e9566e0afb27d560409183ad3e7
Francisco Ruiz <135028491+franciscoruiiz@users.noreply.github.com>
2025-06-28 20:06:25 -0300

    Tp02 Ruiz Francisco Relaciones y DTO Implementados (#72)
    * conflicto resuelto en application.properties
    
    * TP02 - Relaciones y DTO implementados
    
    * application.properties eliminado
    
    * Correcciones del profesor sobre application.properties
    
    ---------
    
    Co-authored-by: Mario Ahumada <ahumadamob@gmail.com>

--- src/main/java/com/imb2025/smedico/service/IResultadoEstudioService.java
+++ src/main/java/com/imb2025/smedico/service/IResultadoEstudioService.java
@@ -2,6 +2,7 @@ package com.imb2025.smedico.service;
 
 import java.util.List;
 
+import com.imb2025.smedico.dto.ResultadoEstudioRequestDTO;
 import com.imb2025.smedico.entity.ResultadoEstudio;
 
 public interface IResultadoEstudioService {
@@ -9,6 +10,8 @@ public interface IResultadoEstudioService {
 	
 	public List<ResultadoEstudio> findAll();
 	public ResultadoEstudio findById(long id);
-	public ResultadoEstudio save(ResultadoEstudio resultadoEstudio);
+	public ResultadoEstudio create(ResultadoEstudio resultadoEstudio);
+	public ResultadoEstudio update(long id, ResultadoEstudio resultadoEstudio) throws Exception;
 	public void deleteById(long id);
+	public ResultadoEstudio fromDto(ResultadoEstudioRequestDTO requestDto) throws Exception;
 }
