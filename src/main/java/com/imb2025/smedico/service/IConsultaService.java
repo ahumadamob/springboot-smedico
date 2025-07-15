76b0c50530c32d7a126ceb3fde9c60b57567f1ec
Nicolas Araya <nicoaraya01@gmail.com>
2025-07-14 22:20:00 -0300

    tp03: Correcciones

--- src/main/java/com/imb2025/smedico/service/IConsultaService.java
+++ src/main/java/com/imb2025/smedico/service/IConsultaService.java
@@ -1,11 +1,14 @@
 package com.imb2025.smedico.service;
 
+import com.imb2025.smedico.dto.ConsultaRequestDTO;
 import com.imb2025.smedico.entity.Consulta;
+
 import java.util.List;
 
 public interface IConsultaService {
     List<Consulta> findAll();
     Consulta findById(Long id);
+    boolean existsById(Long id);
     Consulta save(ConsultaRequestDTO dto);
     Consulta update(Long id, ConsultaRequestDTO dto);
     void deleteById(Long id);
