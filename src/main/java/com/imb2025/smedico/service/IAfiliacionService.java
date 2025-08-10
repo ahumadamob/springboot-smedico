b86f2136cc1bdd02144ff7435e42e48bbf1dd767
Mario Ahumada <ahumadamob@gmail.com>
2025-08-10 19:32:40 -0300

    refactor: standardize additional services

--- src/main/java/com/imb2025/smedico/service/IAfiliacionService.java
+++ src/main/java/com/imb2025/smedico/service/IAfiliacionService.java
@@ -1,15 +1,24 @@
 package com.imb2025.smedico.service;
 
 import java.util.List;
+
+import com.imb2025.smedico.dto.AfiliacionRequestDto;
 import com.imb2025.smedico.entity.Afiliacion;
 
-
 public interface IAfiliacionService {
+
     List<Afiliacion> findAll();
+
     Afiliacion findById(Long id);
-    boolean existsById(Long id); 
-    Afiliacion save(Afiliacion afiliacion);
-    Afiliacion update(Long id, Afiliacion afiliacion);
+
+    Afiliacion create(Afiliacion afiliacion) throws Exception;
+
+    Afiliacion update(Long id, Afiliacion afiliacion) throws Exception;
+
     void deleteById(Long id);
+
+    Afiliacion fromDto(AfiliacionRequestDto dto) throws Exception;
+
+    boolean existsById(Long id);
 
 }
