a6caa80b541c9a20f4d35bf1f337d73a4adf0ee5
Mario Ahumada <ahumadamob@gmail.com>
2025-08-10 10:14:49 -0300

    Rename Encuesta entity to EncuestaSatisfaccion (#134)

--- src/main/java/com/imb2025/smedico/service/IEncuestaService.java
+++ src/main/java/com/imb2025/smedico/service/IEncuestaService.java
@@ -3,17 +3,17 @@ package com.imb2025.smedico.service;
 import java.util.List;
 
 import com.imb2025.smedico.dto.EncuestaRequestDTO;
-import com.imb2025.smedico.entity.Encuesta;
+import com.imb2025.smedico.entity.EncuestaSatisfaccion;
 
 
     public interface IEncuestaService {
-        public List<Encuesta> findAll();
-        public Encuesta findById(Long id);
-        public Encuesta create(Encuesta encuesta);
-        public Encuesta update(Long id, Encuesta encuesta) throws Exception;
-        public Encuesta fromDto(EncuestaRequestDTO dto) throws Exception;
+        public List<EncuestaSatisfaccion> findAll();
+        public EncuestaSatisfaccion findById(Long id);
+        public EncuestaSatisfaccion create(EncuestaSatisfaccion encuesta);
+        public EncuestaSatisfaccion update(Long id, EncuestaSatisfaccion encuesta) throws Exception;
+        public EncuestaSatisfaccion fromDto(EncuestaRequestDTO dto) throws Exception;
         public void deleteById(Long id);
-		public boolean existsById(Long id);
+                public boolean existsById(Long id);
 
 
 
