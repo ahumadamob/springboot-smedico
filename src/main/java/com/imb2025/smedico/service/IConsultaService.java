32957911c28a21f6a82d3ab2ace5a879ccd4ff97
Nicolás Araya <99225767+Nicoaray@users.noreply.github.com>
2025-08-10 00:40:04 -0300

    TP03: HTTP y errores (#95)
    * TP03: HTTP y errores
    
    * tp03: Correcciones

--- src/main/java/com/imb2025/smedico/service/IConsultaService.java
+++ src/main/java/com/imb2025/smedico/service/IConsultaService.java
@@ -1,13 +1,16 @@
 package com.imb2025.smedico.service;
 
+import com.imb2025.smedico.dto.ConsultaRequestDTO;
 import com.imb2025.smedico.entity.Consulta;
+
 import java.util.List;
 
 public interface IConsultaService {
     List<Consulta> findAll();
     Consulta findById(Long id);
-    Consulta save(Consulta consulta);
-    Consulta update(Consulta consulta);
+    boolean existsById(Long id);
+    Consulta save(ConsultaRequestDTO dto);
+    Consulta update(Long id, ConsultaRequestDTO dto);
     void deleteById(Long id);
 }
 
