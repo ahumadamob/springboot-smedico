dbb934fe56ee1a5039a82e8fae57ee0fd6eb1430
Marcelo Cardozo <109989115+MarceloCardozo46@users.noreply.github.com>
2025-08-22 18:23:28 -0300

    Integrar excepciones y DTO genéricos en Factura

--- src/main/java/com/imb2025/smedico/controller/FacturaController.java
+++ src/main/java/com/imb2025/smedico/controller/FacturaController.java
@@ -1,9 +1,11 @@
 package com.imb2025.smedico.controller;
 
+import com.imb2025.smedico.dto.ApiResponseSuccessDto;
 import com.imb2025.smedico.dto.FacturaRequestDto;
 import com.imb2025.smedico.entity.Factura;
 import com.imb2025.smedico.service.IFacturaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,42 +19,49 @@ public class FacturaController {
     private IFacturaService facturaService;
 
     @GetMapping
-    public ResponseEntity<List<Factura>> findAll(){
+    public ResponseEntity<ApiResponseSuccessDto<List<Factura>>> findAll(){
         List<Factura> facturas = facturaService.findAll();
+        String mensaje;
         if(facturas.isEmpty()){
-            return ResponseEntity.noContent().build();
+            mensaje = "No hay facturas disponibles";
+        } else {
+            mensaje = "Lista de Facturas obtenidas obtenidas correctamente";
         }
-        return ResponseEntity.ok(facturas);
+        ApiResponseSuccessDto<List<Factura>> facturasResponse =
+                new ApiResponseSuccessDto<>(true, mensaje, facturas);
+        return ResponseEntity.ok(facturasResponse);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Factura> findById(@PathVariable Long id){
+    public ResponseEntity<ApiResponseSuccessDto<Factura>> findById(@PathVariable Long id){
         Factura factura = facturaService.findById(id);
-        if(factura == null){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(factura);
+        ApiResponseSuccessDto<Factura> facturaResponse =
+                new ApiResponseSuccessDto<>(true, "Factura con id: " + id + " obtenida correctamente", factura);
+        return ResponseEntity.ok(facturaResponse);
     }
 
     @PostMapping
-    public ResponseEntity<Factura> createFactura(@RequestBody FacturaRequestDto requestDTO) throws Exception{
-        return ResponseEntity.ok(facturaService.create(facturaService.fromDto(requestDTO)));
+    public ResponseEntity<ApiResponseSuccessDto<Factura>> createFactura(@RequestBody FacturaRequestDto requestDTO) throws Exception{
+        Factura factura = facturaService.create(facturaService.fromDto(requestDTO));
+        ApiResponseSuccessDto<Factura> facturaResponse =
+                new ApiResponseSuccessDto<>(true, "Factura creada correctamente", factura);
+        return ResponseEntity.status(HttpStatus.CREATED).body(facturaResponse);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Factura> updateFactura(@PathVariable Long id, @RequestBody FacturaRequestDto requestDTO) throws Exception{
-        return ResponseEntity.ok(facturaService.update(id, facturaService.fromDto(requestDTO)));
+    public ResponseEntity<ApiResponseSuccessDto<Factura>> updateFactura(@PathVariable Long id, @RequestBody FacturaRequestDto requestDTO) throws Exception{
+        Factura factura = facturaService.update(id, facturaService.fromDto(requestDTO));
+        ApiResponseSuccessDto<Factura> facturaResponse =
+                new ApiResponseSuccessDto<>(true, "Factura actualizada correctamente", factura);
+        return ResponseEntity.ok(facturaResponse);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteFactura(@PathVariable Long id) throws Exception{
+    public ResponseEntity<ApiResponseSuccessDto<String>> deleteFactura(@PathVariable Long id) throws Exception{
         facturaService.deleteById(id);
-        return ResponseEntity.ok("La factura con el ID: " + id + "fue eliminado correctamente.");
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e){
-        return ResponseEntity.badRequest().body(e.getMessage());
+        ApiResponseSuccessDto<String> facturaResponse =
+                new ApiResponseSuccessDto<>(true, "La factura con el ID: " + id + "fue eliminado correctamente.", "Factura ID: " + id);
+        return ResponseEntity.ok(facturaResponse);
     }
 
 }
