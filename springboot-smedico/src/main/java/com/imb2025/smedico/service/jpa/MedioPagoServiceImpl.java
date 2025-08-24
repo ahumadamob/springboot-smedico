c719030e754d629004c075e6ced45845ce8f82ed
marquuus721 <145239705+marquuus721@users.noreply.github.com>
2025-08-23 22:24:51 -0300

    Integrar excepciones y DTO genéricos en MedioPago

---
+++ springboot-smedico/src/main/java/com/imb2025/smedico/service/jpa/MedioPagoServiceImpl.java
@@ -0,0 +1,63 @@
+package com.imb2025.smedico.service.jpa;
+
+import com.imb2025.smedico.dto.MedioPagoRequestDto;
+import com.imb2025.smedico.entity.MedioPago;
+import com.imb2025.smedico.exception.ResourceNotFoundException;
+import com.imb2025.smedico.repository.MedioPagoRepository;
+import com.imb2025.smedico.service.IMedioPagoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+ 
+@Service
+public class MedioPagoServiceImpl implements IMedioPagoService {
+        @Autowired
+        private MedioPagoRepository repo;
+
+        @Override
+        public List<MedioPago> findAll() {
+                return repo.findAll(); 
+        }
+	
+	
+	@Override
+        public MedioPago findById(Long id) {
+                	return repo.findById(id)//c
+                		    .orElseThrow(() -> new ResourceNotFoundException(//c
+                		        "Entidad no encontrada, id: " + id)); //c
+        }
+
+        @Override
+        public boolean existsById(Long id) {
+                return repo.existsById(id);
+        }
+
+	@Override
+        public MedioPago create(MedioPago medioPago) {
+                return repo.save(medioPago);
+        }
+	
+	@Override
+        public MedioPago update(Long id, MedioPago medioPago) {
+                if (repo.existsById(id)) {
+                        medioPago.setId(id);
+                        return repo.save(medioPago);
+                }
+                return repo.findById(id)//c
+            		    .orElseThrow(() -> new ResourceNotFoundException(//c
+            		        "No se pudo actualizar. Entidad no encontrada de id " + id)); //c
+        } 
+	
+	@Override
+	public void deleteById(Long id) {
+		repo.deleteById(id);
+	}
+	
+        @Override
+        public MedioPago fromDto(MedioPagoRequestDto dto) {
+                MedioPago medioPago = new MedioPago();
+                medioPago.setNombre(dto.getNombre());
+                medioPago.setTipo(dto.getTipo());
+                return medioPago;
+        }
+}
