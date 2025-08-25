ff7dae25bdcb8a0e0e4826210102a6a31279e4c6
Santiago Comes <82416768+Scomes02@users.noreply.github.com>
2025-08-25 20:58:12 -0300

    Tp04 comes santiago (#180)
    * Integrar excepciones y DTO genéricos en TuEntidad
    
    * Integrar excepcioners y DTO genéricos en Asistente
    
    ---------
    
    Co-authored-by: GermanGiorgis <germanggiorgis@gmail.com>

--- src/main/java/com/imb2025/smedico/controller/AsistenteController.java
+++ src/main/java/com/imb2025/smedico/controller/AsistenteController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.imb2025.smedico.dto.ApiResponseSuccessDto;
 import com.imb2025.smedico.dto.AsistenteRequestDto;
 import com.imb2025.smedico.entity.Asistente;
 import com.imb2025.smedico.service.IAsistenteService;
@@ -26,9 +28,15 @@ public class AsistenteController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Asistente> findById(@PathVariable Long id) {
-        Asistente asistente = service.findById(id);            
-        return ResponseEntity.ok(asistente);                   
+    public ResponseEntity<ApiResponseSuccessDto<Asistente>> findById(@PathVariable Long id) {
+        Asistente asistente = service.findById(id);
+
+        ApiResponseSuccessDto<Asistente> resp = new ApiResponseSuccessDto<>();
+        resp.setSuccess(true);
+        resp.setMessage("Asistente encontrado con éxito");
+        resp.setData(asistente);
+
+        return ResponseEntity.ok(resp);
     }
 
     @PostMapping
@@ -48,9 +56,15 @@ public class AsistenteController {
 
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        service.deleteById(id);                                
-        return ResponseEntity.ok().build();                  
+    public ResponseEntity<ApiResponseSuccessDto<Void>> delete(@PathVariable Long id) {
+        service.deleteById(id);
+
+        ApiResponseSuccessDto<Void> resp = new ApiResponseSuccessDto<>();
+        resp.setSuccess(true);
+        resp.setMessage("Asistente eliminado con éxito");
+        resp.setData(null);
+
+        return ResponseEntity.ok(resp);
     }
 
     @ExceptionHandler(Exception.class)
