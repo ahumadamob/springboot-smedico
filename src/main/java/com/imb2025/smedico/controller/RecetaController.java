2d4457c896bdb8bf18abbffb0d59af5b5eaa7ecb
Mauricio Aiello <137466562+MauricioAiello99@users.noreply.github.com>
2025-08-26 20:41:56 -0300

    Integrar excepciones y DTO genéricos en Receta (#159)

--- src/main/java/com/imb2025/smedico/controller/RecetaController.java
+++ src/main/java/com/imb2025/smedico/controller/RecetaController.java
@@ -3,9 +3,9 @@ package com.imb2025.smedico.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.DeleteMapping;	
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.imb2025.smedico.service.IRecetaService;
+import com.imb2025.smedico.dto.ApiResponseSuccessDto;
 import com.imb2025.smedico.dto.RecetaRequestDto;
 import com.imb2025.smedico.entity.Receta;
 
@@ -24,46 +25,51 @@ public class RecetaController {
 	private IRecetaService service;
 	
     @GetMapping("/receta")
-    public ResponseEntity<List<Receta>> findAllReceta() {
+    public ResponseEntity<ApiResponseSuccessDto<List<Receta>>>findAllReceta() {
         List<Receta> lista = service.findAll();
+        ApiResponseSuccessDto<List<Receta>> resp;
         if (lista.isEmpty()) {
-            return ResponseEntity.noContent().build();
+            resp = new ApiResponseSuccessDto<>(true,"No hay recetas disponibles",lista);
+        }else {
+            resp = new ApiResponseSuccessDto<>(true,"Lista de recetas",lista);
         }
-        return ResponseEntity.ok(lista);
-        
+        return ResponseEntity.ok(resp);       
     }
 	
-	@GetMapping("/receta/existe/{id}")
-	public ResponseEntity<Void> existsById(@PathVariable("id") Long id) {
-	    if (service.existsById(id)) {
-	        return ResponseEntity.ok().build();       
-	    } else {
-	        return ResponseEntity.notFound().build();
-	    }
+	@GetMapping("/receta/{id}")
+	public ResponseEntity<ApiResponseSuccessDto<Receta>> existsById(@PathVariable("id") Long id) {
+		Receta receta = service.findById(id);
+		ApiResponseSuccessDto<Receta> resp =
+				new ApiResponseSuccessDto<>(true,"Receta encontrada",receta);
+		return ResponseEntity.ok(resp);
+	  
 	}
 	@PostMapping("/receta")
-	public ResponseEntity<Receta> create(@RequestBody RecetaRequestDto recetaRequestDto) throws Exception {
-        Receta nueva = service.create(service.fromDto(recetaRequestDto));
-        return ResponseEntity.ok(nueva);
+	public ResponseEntity<ApiResponseSuccessDto<Receta>> create(@RequestBody RecetaRequestDto recetaRequestDto) throws Exception {
+        Receta receta = service.create(service.fromDto(recetaRequestDto));
+        ApiResponseSuccessDto<Receta> resp =
+				new ApiResponseSuccessDto<>(true,"Receta creada correctamente",receta);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resp);
     }
 	
 	@PutMapping("/receta/{idreceta}")
-	public ResponseEntity<Receta> update(@PathVariable("idreceta") Long id,
+	public ResponseEntity<ApiResponseSuccessDto<Receta>> update(@PathVariable("idreceta") Long id,
 	        @RequestBody RecetaRequestDto recetaRequestDto) throws Exception {
-	    Receta entity = service.fromDto(recetaRequestDto);
-	    return ResponseEntity.ok(service.update(id, entity));
+        Receta recetaEntity = service.fromDto(recetaRequestDto);
+        Receta actualizado = service.update(id, recetaEntity);
+		ApiResponseSuccessDto<Receta> resp =
+				new ApiResponseSuccessDto<>(true,"Receta actualizada",actualizado);
+		return ResponseEntity.ok(resp);
 	}
 	
 	@DeleteMapping ("/receta/{idreceta}")
-	public ResponseEntity<String> deleteReceta(@PathVariable("idreceta") Long id) {
+	public ResponseEntity<ApiResponseSuccessDto<String>> deleteReceta(@PathVariable("idreceta") Long id) {
         service.deleteById(id);
-        return ResponseEntity.ok("Receta " + id + " eliminada correctamente.");
+        ApiResponseSuccessDto<String> resp= 
+        		new ApiResponseSuccessDto<>(true, "Receta eliminada correctamente", "id: "+id);
+        return ResponseEntity.ok(resp);
     }
-	
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<String> handleException(Exception ex) {
-	  return ResponseEntity.badRequest().body(ex.getMessage());
-	}
+
 	
 }
 
