88c436cfe244501b648ee5bdbd3a799c96fed187
Mauricio Aiello <137466562+MauricioAiello99@users.noreply.github.com>
2025-08-30 16:28:47 -0300

    Validaciones JSR-380 en RecetaDto (#187)

--- src/main/java/com/imb2025/smedico/dto/RecetaRequestDto.java
+++ src/main/java/com/imb2025/smedico/dto/RecetaRequestDto.java
@@ -2,10 +2,19 @@ package com.imb2025.smedico.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+
 public class RecetaRequestDto {
+	@NotNull(message = "La fecha es obligatoria")
+	@PastOrPresent(message = "La fecha no puede ser futura")
     private LocalDate fecha;
+    @Size(min = 10, message = "Las observaciones no pueden tener menos de 10 caracteres")
     private String observaciones;
+    @NotNull(message = "El médico es obligatorio")
     private Long medicoId;
+    @NotNull(message = "El paciente es obligatorio")
     private Long pacienteId;
 
     public RecetaRequestDto() {}
