80acac8496e07c6142e6ec034ab1766c6755506f
GermanGiorgis <germanggiorgis@gmail.com>
2025-09-02 23:14:40 -0300

    Se incorporan las validaciones del Requestdto y se elimina el dto que estaba de mas

--- src/main/java/com/imb2025/smedico/dto/EstadoTurnoRequestDto.java
+++ src/main/java/com/imb2025/smedico/dto/EstadoTurnoRequestDto.java
@@ -1,6 +1,16 @@
 package com.imb2025.smedico.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class EstadoTurnoRequestDto {
+	//Validaciones del tp 05//
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
+    @Pattern(regexp = "^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$", 
+             message = "El nombre solo puede contener letras y espacios")
     private String nombre;
 
     public EstadoTurnoRequestDto() {
