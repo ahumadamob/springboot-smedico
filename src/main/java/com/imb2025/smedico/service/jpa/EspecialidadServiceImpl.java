e16185f8adc24825b9a28452f6b6e851c7ef1126
Gastón Ruiz <135028423+ggastonruiz@users.noreply.github.com>
2025-09-21 12:27:52 -0300

    TP06-ruiz-gaston (#225)
    Co-authored-by: Gaston Fran <gastonjose16@gmail.com>

--- src/main/java/com/imb2025/smedico/service/jpa/EspecialidadServiceImpl.java
+++ src/main/java/com/imb2025/smedico/service/jpa/EspecialidadServiceImpl.java
@@ -42,23 +42,15 @@ public class EspecialidadServiceImpl implements IEspecialidadService{
 	}
 	
 	@Override
-	public Especialidad update(Long id, Especialidad especialidad) throws Exception {	
-	 if (repo.existsById(id)) {
-	        especialidad.setId(id);
-		return repo.save(especialidad);
-		
-	    }else {
-		throw new Exception("No existe esa Especialidad");
+	public Especialidad update(Long id, Especialidad especialidad) {
+	    if (!repo.existsById(id)) {
+	        throw new ResourceNotFoundException("No existe la especialidad con id " + id);
 	    }
-	}	
+	    especialidad.setId(id);
+	    return repo.save(especialidad);
+	}
 	public Especialidad fromDto(EspecialidadRequestDto dto) {
 	
-		    if (dto.getNombre() == null || dto.getNombre().isBlank()) {
-		        throw new IllegalArgumentException("El nombre no puede estar vacío");
-		    }
-		    if (dto.getDescripcion() == null || dto.getDescripcion().isBlank()) {
-		        throw new IllegalArgumentException("La descripción no puede estar vacía");
-		    }
 		    Especialidad especialidad = new Especialidad();
 		    especialidad.setNombre(dto.getNombre());
 		    especialidad.setDescripcion(dto.getDescripcion());
@@ -68,7 +60,7 @@ public class EspecialidadServiceImpl implements IEspecialidadService{
 	@Override
 	public void deleteById(Long id) {
 		if(!repo.existsById(id)) {
-			throw new IllegalArgumentException("La especialidad con id " + id + " no existe");
+			throw new ResourceNotFoundException("La especialidad con id " + id + " no existe");
 		}
 		repo.deleteById(id);
 
