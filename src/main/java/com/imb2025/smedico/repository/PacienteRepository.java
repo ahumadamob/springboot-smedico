4e81789e0c026279ba1d487f454d8dc3db6a6078
Martina <fmartina511@gmail.com>
2025-09-30 10:42:19 -0300

    TP07 - Métodos mágicos en Motivo Cancelacion

--- src/main/java/com/imb2025/smedico/repository/PacienteRepository.java
+++ src/main/java/com/imb2025/smedico/repository/PacienteRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.imb2025.smedico.entity.Paciente;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-	List<Paciente> findByOrderByApellidoAscNombreAsc();
-	List<Paciente> findByDni(String dni);
-	List<Paciente> findByEmailEndingWith(String domain);
-	Long countBy();
+    // Buscar pacientes por apellido
+    List<Paciente> findByApellido(String apellido);
+
+    // Contar pacientes por email
+    long countByEmail(String email);
 }
 
