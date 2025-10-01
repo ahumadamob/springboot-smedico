5b131dd6588fb6ae8c225224cfc654315eb25807
francisco ruiz <franciscojoseruiz19@gmail.com>
2025-09-30 22:32:32 -0300

    tp07-Metodos Magicos en Resultado Estudio

--- src/main/java/com/imb2025/smedico/service/IResultadoEstudioService.java
+++ src/main/java/com/imb2025/smedico/service/IResultadoEstudioService.java
@@ -2,6 +2,8 @@ package com.imb2025.smedico.service;
 
 import com.imb2025.smedico.dto.ResultadoEstudioRequestDto;
 import com.imb2025.smedico.entity.ResultadoEstudio;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface IResultadoEstudioService {
@@ -12,4 +14,7 @@ public interface IResultadoEstudioService {
     public boolean existsById(Long id);
     public void deleteById(Long id);
     public ResultadoEstudio fromDto(ResultadoEstudioRequestDto resultadoEstudioRequestDto);
+    
+    public List<ResultadoEstudio> findByFecha(LocalDate fecha);
+	public long countByFecha(LocalDate fecha);
 }
