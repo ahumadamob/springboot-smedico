af2402211d28193a77912bfc28b3ba06583621a6
Valenii <valentinaferrey162@gmail.com>
2025-10-09 14:40:55 -0300

    Refactor de DTOs y Mapper en Spring Boot

--- src/main/java/com/imb2025/smedico/entity/DetalleReceta.java
+++ src/main/java/com/imb2025/smedico/entity/DetalleReceta.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import com.imb2025.smedico.entity.DetalleReceta;
 
 @Entity
 @Table(name = "detalle_receta")
