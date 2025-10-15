c5fdeb75c01ad3a63cc2a07d839d1e2aaa3c7edb
Marcelo Cardozo <109989115+MarceloCardozo46@users.noreply.github.com>
2025-10-15 16:53:17 -0300

    TP08 - Refactor de DTOs y Mapper en Spring Boot

--- src/main/java/com/imb2025/smedico/service/IFacturaService.java
+++ src/main/java/com/imb2025/smedico/service/IFacturaService.java
@@ -1,6 +1,5 @@
 package com.imb2025.smedico.service;
 
-import com.imb2025.smedico.dto.FacturaRequestDto;
 import com.imb2025.smedico.entity.Factura;
 import java.util.List;
 
@@ -11,7 +10,6 @@ public interface IFacturaService {
     public Factura findById(Long id);
     public boolean existsById(Long id);
     public void deleteById(Long id);
-    public Factura fromDto(FacturaRequestDto facturaRequestDto);
     public List<Factura> findAllByPacienteId(Long id);
     public Long countByMedioPago(String medioPago);
 }
