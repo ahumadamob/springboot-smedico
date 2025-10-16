560546e351f44e383e772ba857c9db30569ad342
Marcos Quispe <145239705+marquuus721@users.noreply.github.com>
2025-10-16 02:06:12 -0300

    Update IMedioPagoService.java

--- src/main/java/com/imb2025/smedico/service/IMedioPagoService.java
+++ src/main/java/com/imb2025/smedico/service/IMedioPagoService.java
@@ -3,7 +3,6 @@ package com.imb2025.smedico.service;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
-import com.imb2025.smedico.dto.request.MedioPagoRequestDto;
 import com.imb2025.smedico.entity.MedioPago;
 import com.imb2025.smedico.entity.MedioPago.TipoPago;
 
@@ -12,11 +11,10 @@ public interface IMedioPagoService {
 	public List<MedioPago> findAll();
 	public MedioPago findById(Long id);
     public MedioPago create(MedioPago medioPago);
-    public MedioPago update(Long id, MedioPago medioPago) throws Exception;
-    public void deleteById(Long id) throws Exception;
-	public MedioPago fromDto(MedioPagoRequestDto dto) throws Exception;  
-	List<MedioPago> findByTipo(TipoPago tipo);//a
-	Long countByNombre(String nombre);//a
+    public MedioPago update(Long id, MedioPago medioPago);
+    public void deleteById(Long id);
+	List<MedioPago> findByTipo(TipoPago tipo);
+	Long countByNombre(String nombre);
 	boolean existsById(Long id);
 	
 }
