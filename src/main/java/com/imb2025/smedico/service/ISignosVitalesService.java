9ea5b8c48d72709c563b005fe67e936551d27ebb
Enzo Juri <149835978+Rafit98@users.noreply.github.com>
2025-10-30 12:47:47 -0300

    Refactor create and update methods in service interface

--- src/main/java/com/imb2025/smedico/service/ISignosVitalesService.java
+++ src/main/java/com/imb2025/smedico/service/ISignosVitalesService.java
@@ -9,8 +9,8 @@ import com.imb2025.smedico.entity.SignosVitales;
 
 public interface ISignosVitalesService {
     List<SignosVitales> findAll();
-    SignosVitales create(SignosVitalesRequestDto dto);
-    SignosVitales update(Long id, SignosVitalesRequestDto dto);
+    SignosVitales create(SignosVitales sig);
+    SignosVitales update(Long id, SignosVitales sig);
     SignosVitales findById(Long id);
     boolean existsById(Long id);
     void deleteById(Long id);
