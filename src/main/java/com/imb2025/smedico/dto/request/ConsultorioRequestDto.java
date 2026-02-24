d6749f95bd3d673fde48baad3ce20ba8b097c2ba
Marianela Zapata <marianelanoelizapatacardinali@gmail.com>
2026-02-24 08:23:01 -0300

    Fix - Ejercicio 2 - Zapata Marianela (#351)
    Co-authored-by: Marianela Zapata <marianelanoelizapatacardinali@gmai.com>

--- src/main/java/com/imb2025/smedico/dto/request/ConsultorioRequestDto.java
+++ src/main/java/com/imb2025/smedico/dto/request/ConsultorioRequestDto.java
@@ -1,4 +1,4 @@
-package com.imb2025.smedico.dto;
+package com.imb2025.smedico.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -16,16 +16,28 @@ public class ConsultorioRequestDto {
     @NotNull(message = "El campo piso es obligatorio")
     @Min(value = 0, message = "El número de piso debe ser mayor o igual a 0")
     private int piso;
+    @NotBlank(message = "El campo identificador es obligatorio")
+    @Size(min = 3, max = 30,message = "El campo combre del consultorio debe tener entre 3 y 30 caracteres")
+    private String identificadorLegible;
 
     public ConsultorioRequestDto() {}
 
-    public ConsultorioRequestDto(String nombre, String ubicacion, int piso) {
+    public ConsultorioRequestDto(String nombre, String ubicacion, int piso, String identificadorLegible) {
         this.nombre = nombre;
         this.ubicacion = ubicacion;
         this.piso = piso;
+        this.identificadorLegible = identificadorLegible;
     }
 
-    public String getNombre() {
+    public String getIdentificadorLegible() {
+		return identificadorLegible;
+	}
+
+	public void setIdentificadorLegible(String identificadorLegible) {
+		this.identificadorLegible = identificadorLegible;
+	}
+
+	public String getNombre() {
         return nombre;
     }
 
---
+++ src/main/java/com/imb2025/smedico/dto/response/ConsultorioResponseDto.java
@@ -0,0 +1,41 @@
+package com.imb2025.smedico.dto.response;
+
+public class ConsultorioResponseDto {
+
+	private String nombre;
+	private String ubicacion;
+	private int piso;
+	private Long version;
+	private String identificadorLegible;
+	
+	public String getIdentificadorLegible() {
+		return identificadorLegible;
+	}
+	public void setIdentificador_legible(String identificadorLegible) {
+		this.identificadorLegible = identificadorLegible;
+	}
+	public Long getVersion() {
+		return version;
+	}
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public int getPiso() {
+		return piso;
+	}
+	public void setPiso(int piso) {
+		this.piso = piso;
+	}
+}
