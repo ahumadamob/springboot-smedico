a28b15a5c7427b0e3e983adeb078fed6549ec478
franciscoruiiz <franciscojoseruiz19@gmail.com>
2026-02-26 21:38:41 -0300

    se agrego repository

--- src/main/java/com/imb2025/smedico/repository/FacturaRepository.java
+++ src/main/java/com/imb2025/smedico/repository/FacturaRepository.java
@@ -4,10 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.imb2025.smedico.entity.Factura;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface FacturaRepository extends JpaRepository<Factura, Long> {
     List<Factura> findAllByPacienteId(Long id);
 
     Long countByMedioPagoNombreIgnoreCase(String medioPago);
+    
+    List<Factura> findByFechaArchivadoIsNull(LocalDate fechaArchivado);
+    
+    List<Factura> findByFechaArchivadoIsNotNull(LocalDate fechaArchivado);
 }
